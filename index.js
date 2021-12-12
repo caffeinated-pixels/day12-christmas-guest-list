7376af2d3fd6293c545dfae816ae6c1a78fb3b16
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-12 09:23:18 -0500

    Render guest list on page load

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const input = document.getElementById('input')
+const listContainer = document.getElementById('list-container')
+
 const btn = document.getElementById('btn')
-let guestList = document.getElementById('guest-list')
-let guests = [
+
+const guests = [
   'Partner',
   'Nice Relative 1',
   'Nice Relative 2',
   'Evil Relative',
   'Lonely Neighbour',
 ]
+
+function displayList() {
+  listContainer.textContent = ''
+  const guestList = document.createElement('ul')
+  guestList.className = 'guest-list'
+
+  guests.forEach((guest, i) => {
+    const listItem = document.createElement('li')
+    listItem.textContent = guest
+    listItem.id = i
+    guestList.appendChild(listItem)
+  })
+  listContainer.appendChild(guestList)
+}
+
+displayList()
+
 // Tasks:
 // - Write the JS to render the Christmas day guest list in the guest-list element.
 // - Add the functionality to add new guests.
